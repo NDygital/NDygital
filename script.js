983799641bf2fd50801c78ab8bdf4e204216ece2
NDygital <shndylhmra29@gmail.com>
2026-07-19 19:08:19 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 const tombol = document.getElementById("openBtn");
-
 const cover = document.getElementById("cover");
-
 const isi = document.getElementById("isi");
 
-tombol.onclick = function(){
-
+tombol.onclick = function () {
     cover.style.display = "none";
-
     isi.style.display = "block";
-
 }
