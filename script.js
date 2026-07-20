a4af068400d01d47a5bb6e31e76ee499638f60ed
NDygital <shndylhmra29@gmail.com>
2026-07-20 13:53:40 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -6,3 +6,46 @@ tombol.onclick = function () {
     cover.style.display = "none";
     isi.style.display = "block";
 }
+
+const slides = document.querySelectorAll(".slide");
+
+let index = 0;
+
+setInterval(function(){
+
+    slides[index].classList.remove("active");
+
+    index++;
+
+    if(index >= slides.length){
+
+        index = 0;
+
+    }
+
+    slides[index].classList.add("active");
+
+},3000);
+
+const targetDate = new Date("December 11, 2026 08:00:00").getTime();
+
+setInterval(function(){
+
+    const now = new Date().getTime();
+
+    const distance = targetDate - now;
+
+    const days = Math.floor(distance / (1000*60*60*24));
+
+    const hours = Math.floor((distance % (1000*60*60*24)) / (1000*60*60));
+
+    const minutes = Math.floor((distance % (1000*60*60)) / (1000*60));
+
+    const seconds = Math.floor((distance % (1000*60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+},1000);
